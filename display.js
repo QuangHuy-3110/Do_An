e7ba9aebdfa125d6ec14f193046c140223ae6ed0
QuangHuy-3110 <quanghuy.0511204@gmail.com>
2024-10-12 13:37:48 +0700

    Update display.js

--- display.js
+++ display.js
@@ -1,35 +1,39 @@
 let reset_2 = document.getElementById("reset_2");
 let table_h = document.getElementById("table_h");
 let table_g = document.getElementById("table_g");
+let table_not_g = document.getElementById("table_not_g");
+let div_h = document.getElementById("div_h");
 
 div_2.style.display = 'block';
 div_3.style.display = 'none';
+table_not_g.style.display = 'none';
 
-let form_of_calculate = () =>{
+function form_of_calculate (){
     let math = document.getElementById("math").value;
-    if (math === "UCS" && math === "Leo_doi"){
-        table_h.style.display = 'none';
-        table_g.style.display = 'block';
-
-    }
-    else if(math === "Gready"){
-        table_h.style.display = 'block';
+    if (math === "Gready" || math === "Leo_doi"){
+        div_h.style.display = 'block';
+        table_not_g.style.display = 'block';
         table_g.style.display = 'none';
     }
-    else {
-        table_h.style.display = 'block';
+    if(math === "UCS"){
+        div_h.style.display = 'none';
+        table_not_g.style.display = 'none';
         table_g.style.display = 'block';
+    }
+    if(math === "A*"){
+        div_h.style.display = 'block';
+        table_g.style.display = 'block';
+        table_not_g.style.display = 'none';
     }
 }
 addEventListener("change", form_of_calculate);
 
-let form_change = () =>{
+function form_change (){
     let type_data = document.getElementById("type_of_data").value;
     if(type_data === "write" ){
         div_2.style.display = 'block';
         div_3.style.display = 'none';        
     }
-
     else{
         div_2.style.display = 'none';
         div_3.style.display = 'block';
@@ -38,14 +42,15 @@ let form_change = () =>{
 addEventListener("change", form_change);
 
 const input_top =  document.getElementById("so_dinh");
+
 function changeTableInput_Top () {
     let table_h = document.getElementById("table_h");
     let dinh = document.getElementById("so_dinh").value;
     for (let i = 0; i<dinh; i++){
         table_h.innerHTML +=
             `<tr>
-                <td><input type="text" name="dinh"></td>
-                <td><input type="number" name="khoang_cach_h"></td>
+                <td><input type="text" name="dinh" class="form-control"></td>
+                <td><input type="number" name="khoang_cach_h" class="form-control"></td>
             </tr>`
     }   
 }
@@ -58,9 +63,9 @@ function changeTableInput_Road () {
     for(let i = 0; i<cung; i++){
         table_g.innerHTML +=
             `<tr>
-                <td><input type="text" name="tu"></td>
-                <td><input type="text" name="den"></td>
-                <td><input type="number" name="khoang_cach"></td>
+                <td><input type="text" name="tu" class="form-control"></td>
+                <td><input type="text" name="den" class="form-control"></td>
+                <td><input type="number" name="khoang_cach" class="form-control"></td>
             </tr>`
     }
 }
